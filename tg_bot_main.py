2ec1b73df26a8966ef381b81baa2bad3f50a0919
devlinium-md <manaf6552@gmail.com>
2022-09-28 15:35:07 +0300

    version 0.022

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -9,21 +9,22 @@ logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(me
 bot = telebot.TeleBot(str(open("api.key", "r").readline()))
 afk_mode = scr_mode = 0
 
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(chat_id=message.chat.id, text='Main menu', reply_markup=kb.menu)
+
+
 try:
-    @bot.message_handler(commands=['start'])
-    def start_message(message):
-        bot.send_message(chat_id=message.chat.id, text='Main menu', reply_markup=kb.menu)
-
-
     @bot.callback_query_handler(func=lambda call: True)
     def callback_worker(call):
         global afk_mode, scr_mode
         # Run WoW client from launcher
         match call.data:
             case "run_wow":
+                bot.send_message(chat_id=call.message.chat.id, text='Trying to launch WoW, please wait')
                 text_in_message = run.start_wow()
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                      text=text_in_message)
+                bot.send_message(chat_id=call.message.chat.id, text=text_in_message)
                 bot.send_message(chat_id=call.message.chat.id, text='Main Menu', reply_markup=kb.menu)
             case "screenshot_menu":
                 if scr_mode == 0:
@@ -33,16 +34,18 @@ try:
                     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                           text="Screenshot Menu", reply_markup=kb.screenshot_menu1)
             case "screenshot_on":
-                bot.send_message(chat_id=call.message.chat.id, text="Пока ещё не работает. Не нажимай!!!",
-                                 reply_markup=kb.main_menu)
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text="Пока ещё не работает. Не нажимай!!!", reply_markup=kb.main_menu)
+                bot.send_message(chat_id=call.message.chat.id, text='Main menu', reply_markup=kb.menu)
                 # text_in_message = run.start_afk()
                 # scr_mode = 1
                 # bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                 #                       text=text_in_message)
                 # bot.send_message(chat_id=call.message.chat.id, text="No AFK menu", reply_markup=kb.no_afk_menu1)
             case "screenshot_off":
-                bot.send_message(chat_id=call.message.chat.id, text="Пока ещё не работает. Не нажимай!!!",
-                                 reply_markup=kb.main_menu)
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text="Пока ещё не работает. Не нажимай!!!", reply_markup=kb.main_menu)
+                bot.send_message(chat_id=call.message.chat.id, text='Main menu', reply_markup=kb.menu)
                 # bot.send_message(chat_id=call.message.chat.id,
                 #                  text="No AFK mode will be disabled on next Jump attempt. "
                 #                       "Please wait")
@@ -55,11 +58,11 @@ try:
                                       text=texts.screenshot)
                 bot.send_photo(chat_id=call.message.chat.id, photo=run.screenshot())
                 if scr_mode == 0:
-                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                          text="Screenshot Menu", reply_markup=kb.screenshot_menu0)
+                    bot.send_message(chat_id=call.message.chat.id, text="Screenshot Menu",
+                                     reply_markup=kb.screenshot_menu0)
                 else:
-                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                          text="Screenshot Menu", reply_markup=kb.screenshot_menu1)
+                    bot.send_message(chat_id=call.message.chat.id, text="Screenshot Menu",
+                                     reply_markup=kb.screenshot_menu1)
             case "no_afk":
                 if afk_mode == 0:
                     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
