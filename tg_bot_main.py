bd0fc3313b2a00edb3bfb388389e964dfacdecbe
devlinium-md <manaf6552@gmail.com>
2022-09-28 15:19:38 +0300

    version 0.020

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -4,11 +4,11 @@ import telebot
 import win_run_events as run
 import logging
 
-
 logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 bot = telebot.TeleBot(str(open("api.key", "r").readline()))
-afk_mode = 0
+afk_mode = scr_mode = 0
+
 try:
     @bot.message_handler(commands=['start'])
     def start_message(message):
@@ -17,43 +17,72 @@ try:
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_worker(call):
-        global afk_mode
+        global afk_mode, scr_mode
         # Run WoW client from launcher
-        if call.data == "run_wow":
-            message = run.start_wow()
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message)
-            bot.send_message(chat_id=call.message.chat.id, text='Main Menu', reply_markup=kb.menu)
-        elif call.data == "screenshot":
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=texts.screenshot)
-            bot.send_photo(chat_id=call.message.chat.id, photo=run.screenshot())
-            bot.send_message(chat_id=call.message.chat.id, text='Main Menu', reply_markup=kb.menu)
-        elif call.data == "no_afk":
-            if afk_mode == 0:
+        match call.data:
+            case "run_wow":
+                text_in_message = run.start_wow()
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                  text="No AFK menu", reply_markup=kb.no_afk_menu1)
-            else:
+                                      text=text_in_message)
+                bot.send_message(chat_id=call.message.chat.id, text='Main Menu', reply_markup=kb.menu)
+            case "screenshot_menu":
+                if scr_mode == 0:
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text="Screenshot Menu", reply_markup=kb.screenshot_menu0)
+                else:
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text="Screenshot Menu", reply_markup=kb.screenshot_menu1)
+            case "screenshot_on":
+                text_in_message = run.start_afk()
+                scr_mode = 1
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                      text="No AFK menu", reply_markup=kb.no_afk_menu2)
-        elif call.data == "main_menu":
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Main Menu",
-                                  reply_markup=kb.menu)
-        elif call.data == "start_no_afk":
-            message = run.start_afk()
-            afk_mode = 1
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message)
-            bot.send_message(chat_id=call.message.chat.id, text="No AFK menu", reply_markup=kb.no_afk_menu2)
-        elif call.data == "stop_no_afk":
-            bot.send_message(chat_id=call.message.chat.id, text="No AFK mode will be disabled on next Jump attempt. "
-                                                                "Please wait")
-            message = run.stop_afk()
-            afk_mode = 0
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message)
-            bot.send_message(chat_id=call.message.chat.id, text="No AFK menu", reply_markup=kb.no_afk_menu1)
-        elif call.data == "enter":
-            message = "Enter is pressed"
-            run.enter()
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message)
-            bot.send_message(chat_id=call.message.chat.id, text="Main Menu", reply_markup=kb.menu)
+                                      text=text_in_message)
+                bot.send_message(chat_id=call.message.chat.id, text="No AFK menu", reply_markup=kb.no_afk_menu1)
+            case "screenshot_off":
+                bot.send_message(chat_id=call.message.chat.id,
+                                 text="No AFK mode will be disabled on next Jump attempt. "
+                                      "Please wait")
+                message = run.stop_afk()
+                scr_mode = 0
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message)
+                bot.send_message(chat_id=call.message.chat.id, text="No AFK menu", reply_markup=kb.no_afk_menu0)
+            case "screenshot":
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text=texts.screenshot)
+                bot.send_photo(chat_id=call.message.chat.id, photo=run.screenshot())
+                if scr_mode == 0:
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text="Screenshot Menu", reply_markup=kb.screenshot_menu0)
+                else:
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text="Screenshot Menu", reply_markup=kb.screenshot_menu1)
+            case "no_afk":
+                if afk_mode == 0:
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text="No AFK menu", reply_markup=kb.no_afk_menu0)
+                else:
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text="No AFK menu", reply_markup=kb.no_afk_menu1)
+            case "main_menu":
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text="Main Menu", reply_markup=kb.menu)
+            case "start_no_afk":
+                message = run.start_afk()
+                afk_mode = 1
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message)
+                bot.send_message(chat_id=call.message.chat.id, text="No AFK menu", reply_markup=kb.no_afk_menu1)
+            case "stop_no_afk":
+                bot.send_message(chat_id=call.message.chat.id, text="No AFK mode will be disabled on next Jump "
+                                                                    "attempt. Please wait")
+                message = run.stop_afk()
+                afk_mode = 0
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message)
+                bot.send_message(chat_id=call.message.chat.id, text="No AFK menu", reply_markup=kb.no_afk_menu0)
+            case "enter":
+                message = "Enter is pressed"
+                run.enter()
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message)
+                bot.send_message(chat_id=call.message.chat.id, text="Main Menu", reply_markup=kb.menu)
 except Exception:
     pass
 
--- tg_keyboards.py
+++ tg_keyboards.py
@@ -4,21 +4,30 @@ import telebot
 
 run_wow = telebot.types.InlineKeyboardButton(text='Run WoW', callback_data='run_wow')
 screenshot = telebot.types.InlineKeyboardButton(text="Take screenshot", callback_data='screenshot')
+screenshot_menu = telebot.types.InlineKeyboardButton(text="Screenshot Menu", callback_data='screenshot_menu')
 no_afk = telebot.types.InlineKeyboardButton(text="No AFK mode", callback_data='no_afk')
 main_menu = telebot.types.InlineKeyboardButton(text="Main menu", callback_data='main_menu')
 enter = telebot.types.InlineKeyboardButton(text="Press Enter", callback_data='enter')
 start_no_afk = telebot.types.InlineKeyboardButton(text="Start", callback_data="start_no_afk")
 stop_no_afk = telebot.types.InlineKeyboardButton(text="Stop", callback_data="stop_no_afk")
+screenshot_on = telebot.types.InlineKeyboardButton(text="ENABLE auto", callback_data="screenshot_on")
+screenshot_off = telebot.types.InlineKeyboardButton(text="DISABLE auto", callback_data="screenshot_off")
 
 
 
 # Menu library
 
 menu = telebot.types.InlineKeyboardMarkup()
-menu.add(run_wow, screenshot, no_afk)
+menu.add(run_wow, screenshot_menu, no_afk)
+
+screenshot_menu0 = telebot.types.InlineKeyboardMarkup()
+screenshot_menu0.add(screenshot, screenshot_on, main_menu)
+
+screenshot_menu1 = telebot.types.InlineKeyboardMarkup()
+screenshot_menu1.add(screenshot, screenshot_off, main_menu)
+
+no_afk_menu0 = telebot.types.InlineKeyboardMarkup()
+no_afk_menu0.add(enter, start_no_afk, main_menu)
 
 no_afk_menu1 = telebot.types.InlineKeyboardMarkup()
-no_afk_menu1.add(enter, start_no_afk, main_menu)
-
-no_afk_menu2 = telebot.types.InlineKeyboardMarkup()
-no_afk_menu2.add(enter, stop_no_afk, main_menu)
+no_afk_menu1.add(enter, stop_no_afk, main_menu)
